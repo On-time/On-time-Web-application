4b19ef04949105100978c2d7aa1153dca7ed1ca9
Witoon <alive-@msn.com>
2016-11-24 00:05:24 +0700

    Add test hub class

---
+++ src/OnTimeWebApplication/Hubs/SubjectHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Microsoft.AspNetCore.SignalR.Hubs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnTimeWebApplication.Hubs
+{
+    public class SubjectHub : Hub
+    {
+        private IHubCallerConnectionContext<dynamic> _test;
+    }
+}
